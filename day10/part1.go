b1433baf27ea27a7ef25879c0700a7984d40d719
kle <kle@axon.com>
2023-12-27 10:38:59 +0700

    finish day 10 part 1

--- day10/part1.go
+++ day10/part1.go
@@ -3,6 +3,7 @@ package day10
 import (
 	"bufio"
 	"fmt"
+	"github.com/samber/lo"
 	"log"
 	"os"
 	"path/filepath"
@@ -24,8 +25,6 @@ func part1(fileName string) int {
 
 	// Create a scanner to read the file line by line
 	scanner := bufio.NewScanner(file)
-
-	acc := 0
 
 	var tiles [][]string
 
@@ -49,28 +48,46 @@ func part1(fileName string) int {
 
 	fmt.Printf("sTile: %+v\n", sTile)
 
-	sNext := Tile{sTile.H + 1, sTile.V, tiles[sTile.V][sTile.H+1]}
+	sLoc := sTile.toLoc()
 
-	point := Point{sTile, sNext}
-	moves := []string{}
-	canMove := true
-	for point.next.Value != "S" && canMove {
-		nextPoint, canMovee := point.move(&tiles)
-		moves = append(moves, nextPoint.next.Value)
-		point = nextPoint
-		canMove = canMovee
+	sNexts := lo.FilterMap([]Loc{sLoc.up(), sLoc.down(), sLoc.right(), sLoc.left()}, func(loc Loc, _ int) (Tile, bool) {
+		Value, exists := mapp.get(loc.H, loc.V)
+		return Tile{loc.H, loc.V, Value}, exists
+	})
+
+	var moves []string
+
+	for _, sNext := range sNexts {
+		point := Point{sTile, sNext}
+		var thisMoves []string
+		canMove := true
+		for canMove {
+			thisMoves = append(thisMoves, point.next.Value)
+			nextPoint, canMovee := point.move(&mapp)
+			point = nextPoint
+			canMove = canMovee
+		}
+		if point.next.Value == "S" {
+			moves = thisMoves
+			break
+		}
 	}
 
 	fmt.Printf("Move: %+v\n", moves)
-	fmt.Printf("Total: %+v\n", tiles)
+	fmt.Printf("Total: %+v\n", len(moves)/2)
 
-	return acc
+	return len(moves) / 2
 }
 
 type Tile struct {
 	H     int
 	V     int
 	Value string
+}
+
+type Loc struct {
+	H int
+	V int
 }
 
 type Point struct {
@@ -82,6 +99,26 @@ type Map struct {
 	Data    [][]string
 	hLength int
 	vLength int
+}
+
+func (tile *Tile) toLoc() Loc {
+	return Loc{tile.H, tile.V}
+}
+
+func (loc *Loc) right() Loc {
+	return Loc{loc.H + 1, loc.V}
+}
+
+func (loc *Loc) left() Loc {
+	return Loc{loc.H - 1, loc.V}
+}
+
+func (loc *Loc) up() Loc {
+	return Loc{loc.H, loc.V - 1}
+}
+
+func (loc *Loc) down() Loc {
+	return Loc{loc.H, loc.V + 1}
 }
 
 func right(h int, v int) (int, int) {
@@ -108,15 +145,15 @@ func (mapp *Map) get(h int, v int) (string, bool) {
 	}
 }
 
-func (point *Point) move(mapp *[][]string) (Point, bool) {
+func (point *Point) move(mapp *Map) (Point, bool) {
 	nextt, canMove := point.current.move(point.next, mapp)
 	return Point{point.next, nextt}, canMove
 }
 
-func (tile *Tile) move(next Tile, mapp *[][]string) (Tile, bool) {
+func (tile *Tile) move(next Tile, mapp *Map) (Tile, bool) {
 	var nextt Tile
-	var nextH int
-	var nextV int
+	nextH := -1
+	nextV := -1
 	hDelta := next.H - tile.H
 	vDelta := next.V - tile.V
 
@@ -133,36 +170,38 @@ func (tile *Tile) move(next Tile, mapp *[][]string) (Tile, bool) {
 		}
 	case "L":
 		if vDelta == 1 {
-			nextH, nextV = right(next.H, nextV)
+			nextH, nextV = right(next.H, next.V)
 		} else if hDelta == -1 {
-			nextH, nextV = up(next.H, nextV)
+			nextH, nextV = up(next.H, next.V)
 		}
 	case "J":
 		if vDelta == 1 {
-			nextH, nextV = left(next.H, nextV)
+			nextH, nextV = left(next.H, next.V)
 		} else if hDelta == 1 {
-			nextH, nextV = up(next.H, nextV)
+			nextH, nextV = up(next.H, next.V)
 		}
 	case "7":
 		if hDelta == 1 {
-			nextH, nextV = down(next.H, nextV)
+			nextH, nextV = down(next.H, next.V)
 		} else if vDelta == -1 {
-			nextH, nextV = left(next.H, nextV)
+			nextH, nextV = left(next.H, next.V)
 		}
 	case "F":
 		if hDelta == -1 {
-			nextH, nextV = down(next.H, nextV)
+			nextH, nextV = down(next.H, next.V)
 		} else if vDelta == -1 {
-			nextH, nextV = right(next.H, nextV)
+			nextH, nextV = right(next.H, next.V)
 		}
 	}
-	canMove := false
-	if &nextH != nil && &nextV != nil {
-		canMove = true
-		nextValue := (*mapp)[nextV][nextH]
-		nextt = Tile{nextH, nextV, nextValue}
-		return nextt, canMove
+	if nextH >= 0 && nextV >= 0 {
+		nextValue, exists := (*mapp).get(nextH, nextV)
+		if exists {
+			nextt = Tile{nextH, nextV, nextValue}
+			return nextt, true
+		} else {
+			return next, false
+		}
 	} else {
-		return next, canMove
+		return next, false
 	}
 }
--- day10/part1_test.go
+++ day10/part1_test.go
@@ -4,14 +4,21 @@ import "testing"
 
 func TestP1(t *testing.T) {
 	result := part1("input")
-	if result != 1921201931 {
+	if result != 6754 {
 		t.Fail()
 	}
 }
 
 func TestP1T(t *testing.T) {
 	result := part1("test_data")
-	if result != 114 {
+	if result != 4 {
+		t.Fail()
+	}
+}
+
+func TestP1T1(t *testing.T) {
+	result := part1("test_data_1")
+	if result != 8 {
 		t.Fail()
 	}
 }
