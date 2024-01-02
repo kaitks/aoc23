cfd2091eeaec0fdd7236df01a828cb10f23cd944
kle <kle@axon.com>
2024-01-02 20:42:30 +0700

    wip day 13 part2

--- day13/solution.go
+++ day13/solution.go
@@ -21,7 +21,7 @@ func solution(fileName string) int {
 	data := string(raw)
 	areas := lo.Map(strings.Split(data, "\n\n"), func(area string, _ int) Area {
 		rows := strings.Split(area, "\n")
-		rowsV := []string{}
+		var rowsV []string
 		for i := 0; i < len(rows[0]); i++ {
 			rowsV = append(rowsV, strings.Join(lo.Map(rows, func(row string, _ int) string {
 				return string(row[i])
@@ -35,7 +35,6 @@ func solution(fileName string) int {
 	for _, area := range areas {
 		fmt.Printf("Area:\n%+v\n", area.toString)
 		result := process(area)
-		//fmt.Printf("Way To Solve: %+v\n\n", wayToSolve)
 		acc += result
 	}
 
@@ -53,25 +52,56 @@ type Area struct {
 }
 
 func process(area Area) int {
+	sum := 0
+outerLoop:
+	for v := 0; v < area.VLength; v++ {
+		for h := 0; h < area.HLength; h++ {
+			hRows := area.HorizontalRows
+			vRows := area.VerticalRows
+			if hRows[v][h] == '.' {
+				hRows[v] = replaceStringAtIndex(hRows[v], h, "#")
+				vRows[h] = replaceStringAtIndex(vRows[h], v, "#")
+				sum = findSum(hRows, vRows)
+				if sum > 0 {
+					break outerLoop
+				}
+			}
+		}
+	}
+	return sum
+}
+
+func findSum(hRows []string, vRows []string) int {
+	findMirror := memoize(findMirror)
 	possibleVerticalMirrors := mapset.NewSet[int]()
-	for i := 0; i < area.HLength-1; i++ {
+	for i := 0; i < len(hRows[0])-1; i++ {
 		possibleVerticalMirrors.Add(i)
 	}
-	for _, row := range area.HorizontalRows {
+	for _, row := range hRows {
 		mirros := findMirror(row)
 		possibleVerticalMirrors = possibleVerticalMirrors.Intersect(mirros)
+		if possibleVerticalMirrors.Cardinality() == 0 {
+			break
+		}
 	}
-	fmt.Printf("Vertical Mirror: %+v\n", possibleVerticalMirrors.ToSlice())
+	if possibleVerticalMirrors.Cardinality() > 0 {
+		fmt.Printf("Vertical Mirror: %+v\n", possibleVerticalMirrors.ToSlice())
+	}
 
 	possibleHorizontalMirrors := mapset.NewSet[int]()
-	for i := 0; i < area.VLength-1; i++ {
+	for i := 0; i < len(vRows[0])-1; i++ {
 		possibleHorizontalMirrors.Add(i)
 	}
-	for _, row := range area.VerticalRows {
+	for _, row := range vRows {
 		mirros := findMirror(row)
 		possibleHorizontalMirrors = possibleHorizontalMirrors.Intersect(mirros)
+		if possibleHorizontalMirrors.Cardinality() == 0 {
+			break
+		}
 	}
-	fmt.Printf("Horizontal Mirror: %+v\n", possibleHorizontalMirrors.ToSlice())
+	if possibleHorizontalMirrors.Cardinality() > 0 {
+		fmt.Printf("Horizontal Mirror: %+v\n", possibleHorizontalMirrors.ToSlice())
+	}
 
 	vSum := lo.Sum(lo.Map(possibleVerticalMirrors.ToSlice(), func(m int, i int) int {
 		return m + 1
@@ -97,4 +127,24 @@ func findMirror(row string) mapset.Set[int] {
 		}
 	}
 	return mirros
+}
+
+func replaceStringAtIndex(str string, index int, replacement string) string {
+	if index >= 0 && index < len(str) {
+		return str[:index] + replacement + str[index+1:] // Combine parts
+	} else {
+		return str // Handle invalid index
+	}
+}
+
+func memoize(f func(string) mapset.Set[int]) func(string) mapset.Set[int] {
+	cache := make(map[string]mapset.Set[int])
+	return func(x string) mapset.Set[int] {
+		if val, ok := cache[x]; ok {
+			return val
+		}
+		val := f(x)
+		cache[x] = val
+		return val
+	}
 }
