96db609240c6920a2ba8c53011e2d91137ab2b5a
kle <kle@axon.com>
2024-01-24 18:57:21 +0700

    finish day21 p2 brute force

---
+++ day21p2/solution.go
@@ -0,0 +1,121 @@
+package day21p2
+
+import (
+	"fmt"
+	mapset "github.com/deckarep/golang-set/v2"
+	"github.com/gammazero/deque"
+	"github.com/samber/lo"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func solution(fileName string, maxStep int) int {
+	pwd, _ := os.Getwd()
+	// Get the file name from the command line argument
+	filePath := filepath.Join(pwd, fileName)
+	println("Input file:", filePath)
+	println("")
+
+	// Create a scanner to read the file line by line
+	raw, _ := os.ReadFile(filePath)
+	data := string(raw)
+	mapp := parseMap(data)
+	stepQueue := deque.Deque[PosStep]{}
+	seenMap := map[Pos]int{}
+	stepQueue.PushBack(PosStep{Pos: mapp.S, Step: 0})
+
+	remain := maxStep % 2
+
+	for {
+		if stepQueue.Len() == 0 {
+			break
+		}
+		pos := stepQueue.PopFront()
+		posStepRemain := pos.Step % 2
+		if posStepRemain == remain {
+			seenStep, exists := seenMap[pos.Pos]
+			if !exists {
+				seenMap[pos.Pos] = pos.Step
+			} else {
+				if pos.Step < seenStep {
+					seenMap[pos.Pos] = pos.Step
+				} else {
+					continue
+				}
+			}
+		}
+		if pos.Step == maxStep {
+			continue
+		}
+		nextPositions := pos.step()
+		nextPositions = lo.Filter(nextPositions, func(next Pos, _ int) bool {
+			var relativeX int
+			var relativeY int
+			if next.X < 0 {
+				relativeX = mapp.Width + (next.X % mapp.Width)
+			} else {
+				relativeX = next.X % mapp.Width
+			}
+			if next.Y < 0 {
+				relativeY = mapp.Height + (next.Y % mapp.Height)
+			} else {
+				relativeY = next.Y % mapp.Height
+			}
+			return !mapp.Rocks.Contains(Pos{relativeX, relativeY})
+		})
+		for _, next := range nextPositions {
+			stepQueue.PushBack(PosStep{Pos: next, Step: pos.Step + 1})
+		}
+	}
+
+	total := len(seenMap)
+	fmt.Printf("\nTotal: %+v\n", total)
+	return total
+}
+
+type Map struct {
+	Grid   [][]int32
+	Width  int
+	Height int
+	Rocks  mapset.Set[Pos]
+	S      Pos
+}
+
+type Pos struct {
+	X int
+	Y int
+}
+
+type PosStep struct {
+	Pos
+	Step int
+}
+
+func (pos *Pos) step() []Pos {
+	x := pos.X
+	y := pos.Y
+	return []Pos{{x + 1, y}, {x - 1, y}, {x, y + 1}, {x, y - 1}}
+}
+
+func parseMap(data string) *Map {
+	rows := strings.Split(data, "\n")
+	grids := [][]int32{}
+	var S Pos
+	Rocks := mapset.NewSet[Pos]()
+	for y, rowStr := range rows {
+		row := []int32{}
+		for x, char := range rowStr {
+			row = append(row, char)
+			if char == 'S' {
+				S = Pos{x, y}
+			} else if char == '#' {
+				Rocks.Add(Pos{x, y})
+			}
+		}
+		grids = append(grids, row)
+	}
+	Height := len(rows)
+	Width := len(grids[0])
+	return &Map{Grid: grids, Width: Width, Height: Height, Rocks: Rocks, S: S}
+}
---
+++ day21p2/solution_test.go
@@ -0,0 +1,34 @@
+package day21p2
+
+import "testing"
+
+func TestSolution(t *testing.T) {
+	result := solution("input", 64)
+	if result != 3503 {
+		t.Fail()
+	}
+}
+
+func TestSolutionT(t *testing.T) {
+	if solution("test_data", 6) != 16 {
+		t.Fail()
+	}
+	if solution("test_data", 10) != 50 {
+		t.Fail()
+	}
+	if solution("test_data", 50) != 1594 {
+		t.Fail()
+	}
+	if solution("test_data", 100) != 6536 {
+		t.Fail()
+	}
+	if solution("test_data", 500) != 167004 {
+		t.Fail()
+	}
+	if solution("test_data", 1000) != 668697 {
+		t.Fail()
+	}
+	if solution("test_data", 5000) != 16733044 {
+		t.Fail()
+	}
+}
